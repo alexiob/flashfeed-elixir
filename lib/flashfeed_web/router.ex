dfaa8ad492d91f309314e954248bca4e83db0eeb
Alessandro Iob <alessandro.iob@gmail.com>
2020-02-27 10:42:21 +0100

    upgraded liveview to 0.8.0

--- lib/flashfeed_web/router.ex
+++ lib/flashfeed_web/router.ex
@@ -2,10 +2,12 @@ defmodule FlashfeedWeb.Router do
   use FlashfeedWeb, :router
   use Pow.Phoenix.Router
 
+  import Phoenix.LiveView.Router
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug Phoenix.LiveView.Flash
+    plug :fetch_live_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
